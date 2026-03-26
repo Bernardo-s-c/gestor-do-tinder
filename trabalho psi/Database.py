09effb796b6db062a87f9d7fe5664ccafdf64ff5
Bernardo <bernardosantosc2010@gmail.com>
2026-03-26 12:03:51 +0000

    Delete trabalho psi directory

--- trabalho psi/Database.py
+++
@@ -1,7 +0,0 @@
-utilizadores = []
-matches = []
-
-OPCOES_MUSICA = {1: "Pop", 2: "Rock", 3: "Hip-Hop", 4: "Jazz", 5: "Clássica", 6: "Reggae", 7: "Indie", 8: "Metal"}
-OPCOES_HOBBIES = {1: "Ginásio", 2: "Leitura", 3: "Viagens", 4: "Gaming", 5: "Culinária", 6: "Dança", 7: "Séries"}
-OPCOES_ESTETICA = {1: "Loiro/a", 2: "Moreno/a", 3: "Ruivo/a", 4: "Alternativo/a", 5: "Tatuado/a", 6: "Desportivo"}
-OPCOES_GENERO = {1: "Masculino", 2: "Feminino", 3: "Não-Binário"}
