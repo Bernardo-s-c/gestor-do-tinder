7daad72ef083f14e9c9dfc984cd1edb5f2baf57f
Bernardo <bernardosantosc2010@gmail.com>
2026-04-23 10:41:56 +0100

    Update error messages in utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,13 +8,13 @@ OPCOES_GENERO   = {1: "Masculino", 2: "Feminino", 3: "Não-Binário"}
 def validar_inteiro(pergunta):
     while True:
         try: return int(input(pergunta))
-        except: print("[ERRO] Use apenas números.")
+        except: print("[400] Use apenas números.")
 
 def validar_apenas_letras(pergunta):
     while True:
         valor = input(pergunta).strip()
         if valor.replace(" ", "").isalpha(): return valor
-        print("[ERRO] Este campo só pode conter letras.")
+        print("[400] Este campo só pode conter letras.")
 
 def validar_idade_18(pergunta):
     while True:
@@ -22,16 +22,16 @@ def validar_idade_18(pergunta):
             data_str = input(pergunta)
             nasc = datetime.strptime(data_str, "%Y-%m-%d").date()
             idade = (date.today() - nasc).days // 365
-            if idade < 18: print(f"[ERRO] Menor de idade ({idade} anos). Mínimo 18.")
+            if idade < 18: print(f"[422] Menor de idade ({idade} anos). Mínimo 18.")
             else: return nasc
-        except: print("[ERRO] Formato inválido! Use AAAA-MM-DD.")
+        except: print("[400] Formato inválido! Use AAAA-MM-DD.")
 
 def validar_min_max():
     while True:
         i_min = validar_inteiro("Idade Mínima interesse: ")
         i_max = validar_inteiro("Idade Máxima interesse: ")
-        if i_min < 18: print("[ERRO] O interesse mínimo tem de ser 18.")
-        elif i_max < i_min: print("[ERRO] A idade máxima não pode ser menor que a mínima.")
+        if i_min < 18: print("[422] O interesse mínimo tem de ser 18.")
+        elif i_max < i_min: print("[422] A idade máxima não pode ser menor que a mínima.")
         else: return i_min, i_max
 
 def menu_escolha(titulo, dicionario):
@@ -40,4 +40,4 @@ def menu_escolha(titulo, dicionario):
     while True:
         res = validar_inteiro("Escolha o ID: ")
         if res in dicionario: return dicionario[res]
-        print("[ERRO] Escolha um número da lista.")
+        print("[405] Escolha um número da lista.")
